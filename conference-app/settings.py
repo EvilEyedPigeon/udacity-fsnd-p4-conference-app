ac66cc07dec345a7aa29298b3f2cc49d87aeee84
Pedro Tejada <pedro.tejada.314@gmail.com>
2016-03-30 23:26:17 -0600

    fix: Update settings.py to include changes not committed before
    ALLOWED_CLIENT_IDS, AUDIENCES, SCOPES

--- conference-app/settings.py
+++ conference-app/settings.py
@@ -9,6 +9,7 @@ $Id$
 created/forked from conference.py by wesc on 2014 may 24
 
 """
+import endpoints
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
@@ -16,3 +17,9 @@ WEB_CLIENT_ID = 'replace with Web client ID'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
+
+# Default client IDs, audiences, and scopes for the APIs.
+# Usually these do not have to be updated. Just set the client IDs above.
+ALLOWED_CLIENT_IDS = [WEB_CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID, ANDROID_CLIENT_ID, IOS_CLIENT_ID]
+AUDIENCES = [ANDROID_AUDIENCE]
+SCOPES = [endpoints.EMAIL_SCOPE]
